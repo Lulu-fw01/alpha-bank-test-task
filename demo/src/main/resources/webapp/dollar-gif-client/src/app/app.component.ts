ec23a83261d25461a0d95133d841e00198bb0d2d
luka <limarkov@edu.hse.ru>
2022-06-11 02:08:31 +0300

    front v1

--- demo/src/main/resources/webapp/dollar-gif-client/src/app/app.component.ts
+++ demo/src/main/resources/webapp/dollar-gif-client/src/app/app.component.ts
@@ -8,33 +8,26 @@ import {Router} from "@angular/router";
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements  OnInit{
+export class AppComponent implements OnInit {
   title = 'dollar-gif-client';
   rate: Rate | undefined;
 
   constructor(private router: Router, private dollarGifService: DollarGifService) {
   }
 
-  getRate() {
-    this.dollarGifService.getRate("RUB").subscribe(data => {
+  getRate(base: string) {
+    this.dollarGifService.getRate(base).subscribe(data => {
       this.rate = data;
     });
   }
 
-  /*addBook(): void {
-    this.router.navigate(['add-book'])
-      .then((e) => {
-        if (e) {
-          console.log("Navigation is successful!");
-        } else {
-          console.log("Navigation has failed!");
-        }
-      });
-  };
-*/
   ngOnInit(): void {
     this.router.events.subscribe(value => {
-      this.getRate();
+      this.getRate("RUB");
     });
+  }
+
+  onGetGifClick(base: string) {
+    this.getRate(base);
   }
 }
--- demo/src/main/resources/webapp/dollar-gif-client/src/app/app.module.ts
+++ demo/src/main/resources/webapp/dollar-gif-client/src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { GetRateComponent } from './rate/get-rate/get-rate.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GetRateComponent
   ],
   imports: [
     BrowserModule,
