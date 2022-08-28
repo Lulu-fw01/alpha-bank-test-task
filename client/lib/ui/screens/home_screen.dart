1a32d65e1694940d247af3b660bf2d676f14c4db
luka <limarkov@edu.hse.ru>
2022-08-28 21:04:23 +0300

    created flutter client

---
+++ client/lib/ui/screens/home_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Column()),
+    );
+  }
+}
