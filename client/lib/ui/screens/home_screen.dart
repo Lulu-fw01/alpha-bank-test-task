b71f4351f4efaae9848dbd40a857b3b24b2592c2
luka <limarkov@edu.hse.ru>
2022-08-29 00:42:59 +0300

    wip

--- client/lib/ui/screens/home_screen.dart
+++ client/lib/ui/screens/home_screen.dart
@@ -9,10 +9,39 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  String _currencyBase = "";
+  final TextEditingController _currencyBaseController =
+      new TextEditingController();
+
+  @override
+  void initState() {
+    _currencyBaseController.addListener(() {
+      _currencyBase = _currencyBaseController.text;
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Column()),
+      body: Container(
+        padding: EdgeInsets.all(128),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: _currencyBaseController,
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            ElevatedButton(onPressed: () {}, child: Text('Get gif')),
+            const SizedBox(height: 8,),
+            Image.network('https://res.cloudinary.com/practicaldev/image/fetch/s--s2WZChX_--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_66%2Cw_880/https://roszkowski.dev/images/2020-05-04/flutter_logo_leg.gif')
+          ],
+        ),
+      ),
     );
   }
 }
